514ab8695a5599bacb49d5ffb35f574f822fc85e
Ryan <ryansousa.9810@gmail.com>
2021-08-30 15:16:22 -0300

    terceiro commit

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,5 +1,18 @@
 import { Connection, createConnection, getConnectionOptions } from 'typeorm';
 
+interface IOptions {
+    host: string;
+  }
+  
+  getConnectionOptions().then(options => {
+    const newOptions = options as IOptions;
+    newOptions.host = 'database'; //Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados
+    createConnection({
+      ...options,
+    });
+
+  })
+
 export default async (): Promise<Connection> => {
     const defaultOptions = await getConnectionOptions();
 
@@ -7,5 +20,5 @@ export default async (): Promise<Connection> => {
         Object.assign(defaultOptions, {
             host: "database_ignite"
         })
-    );
+    )
 }
